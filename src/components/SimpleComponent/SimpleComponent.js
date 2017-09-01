b8a64f5223ab8352359c81075a026e3e5091089a
Marco Sartiano <marcosartiano@MacBook-Pro-di-Marco.local>
2017-09-01 17:26:04 +0100

    Added jQuery example in SimpleComponent

--- src/components/SimpleComponent/SimpleComponent.js
+++ src/components/SimpleComponent/SimpleComponent.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 export default class SimpleComponent extends Component {
 
+    buttonClicked() {
+        $('.knnz.deleteme').remove();
+    }
+    
     render() {
         return <div>
                 <p>You should be editing and playing with this :-)</p>
                 <p>Come find me! I am the file <span className='knnz'>src/components/SimpleComponent/SimpleComponent.js</span></p>
+                <div>
+                    jQuery example: <span className='knnz deleteme'>THIS WILL BE DELETED WHEN YOU CLICK THE</span>
+                     <button onClick={this.buttonClicked}>button</button>
+                 </div>
             </div>;
     }
 }
